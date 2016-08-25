52ea6d21e5ed6296fc380142c6405123c1c5de95
Gabriel Malet <gabrielmalet@gmail.com>
2016-08-25 15:38:57 +0200

    Changes

--- gop-server/scripts/entrypoint.sh
+++ gop-server/scripts/entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 
 for CONF_FILE in `find /src/nginx/sites-enabled -type f -name "*.conf"`; do
-	sed -i "s/__NGINX_DOMAIN__/$NGINX_DOMAIN/g" $CONF_FILE
+	sed -i -e "s/__NGINX_DOMAIN__/$NGINX_DOMAIN/g" \
+	 	-e "s/__NGINX_SSL_CERT__/$NGINX_SSL_CERT/g" \
+	 	-e "s/__NGINX_SSL_CERT_KEY__/$NGINX_SSL_CERT_KEY/g" $CONF_FILE
 done;
 
 exec "$@"

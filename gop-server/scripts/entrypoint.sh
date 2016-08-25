b7ec520aac61ddf735cdd53dfe031c39a26ea0c2
Gabriel Malet <gabrielmalet@gmail.com>
2016-08-25 15:27:26 +0200

    NginxContainer-less update

--- gop-server/scripts/entrypoint.sh
+++ gop-server/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for CONF_FILE in `find /etc/nginx/sites-enabled -type f -name "*.conf"`; do
+for CONF_FILE in `find /src/nginx/sites-enabled -type f -name "*.conf"`; do
 	sed -i "s/__NGINX_DOMAIN__/$NGINX_DOMAIN/g" $CONF_FILE
 done;
 
